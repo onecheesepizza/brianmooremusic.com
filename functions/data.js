e2e3e538a4ce02f7887f4a61168f78e8cae2bd39
zebranode <52761974+zebranode@users.noreply.github.com>
2021-04-30 18:36:34 -0700

    implement file cache of data in build step, conditional use in API

--- functions/data.js
+++ functions/data.js
@@ -1,56 +1,94 @@
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
+  require('dotenv').config();
 };
 
 // init Airtable
 const airtable = require('airtable').base(process.env.AIRTABLE_BASE_ID);
+const fs = require('fs');
 
 // get data from the Airtable API
 const getAirtableData = async (table, view, fields) => {
-  const tableData = await 
+  const tableData = await
     airtable(table)
-    .select({
-      view: view,
-      fields: fields ? fields : []
+      .select({
+        view: view,
+        fields: fields ? fields : []
       })
-    .all();
+      .all();
   // only return the necessary field data from tableData
   return tableData.map((record) => record.fields);
 };
 
-exports.handler = async function(event, context) {
-    // get data from the Airtable API
-    const data = await getAirtableData('credits', 'Grid view');
+// config
+const config = {
+  location: 'public/build/',
+  fileName: 'data.json'
+};
 
-    // build output object for each post entry
-    const output = data.map(entry => {
+const readDataFile = async (filePath) => {
+  try {
+    // return data from cache file
+    const data = fs.readFileSync(filePath, 'utf-8');
+    return JSON.parse(data);
+  } catch (err) {
+    console.error(err);
+    console.error('cache file not found: reading data from API');
+    // if error, try to return data from API
+    return await getAirtableData('credits', 'Grid view');
+  }
+};
 
-        const creditData = {
-            title: entry.title,
-            credits: entry.credits,
-        };
-        if (entry.youtube) {
-            creditData.youtube = entry.youtube;
-        };
-        if (entry["bandcamp-url"]) {
-            creditData.bandcamp = {
-                url: entry["bandcamp-url"],
-                albumId: entry["bandcamp-id"]
-            }
-        };
-        if (entry["custom-image"]) {
-            creditData.customMedia = {
-                image: entry["custom-image"],
-                audio: entry["custom-audio"] ? entry["custom-audio"].replace(/\s/g, '').split(",") : null
-            }
-        }
-        
-        return creditData;
-    });
+const buildOutputData = (rawData) => {
+  // map raw data to desired output object
+  return rawData.map(entry => {
 
-    // return response object
-    return {
-        statusCode: 200,
-        body: JSON.stringify(output)
+    const creditData = {
+      title: entry.title,
+      credits: entry.credits,
     };
+    if (entry.youtube) {
+      creditData.youtube = entry.youtube;
+    };
+    if (entry["bandcamp-url"]) {
+      creditData.bandcamp = {
+        url: entry["bandcamp-url"],
+        albumId: entry["bandcamp-id"]
+      }
+    };
+    if (entry["custom-image"]) {
+      creditData.customMedia = {
+        image: entry["custom-image"],
+        audio: entry["custom-audio"] ? entry["custom-audio"].replace(/\s/g, '').split(",") : null
+      }
+    }
+
+    return creditData;
+  });
+};
+
+const getData = async (config) => {
+  console.log('process.env.NODE_ENV: ', process.env.NODE_ENV);
+  // get data from cache file in production env, 
+  if (process.env.NODE_ENV === 'production') {
+    console.log('reading data from file');
+    return readDataFile(config.location + config.fileName);
+  // get data from live Airtable API otherwise
+  } else {
+    console.log('reading data from API');
+    return await getAirtableData('credits', 'Grid view');
+  }
+};
+
+exports.handler = async function (event, context) {
+
+  // get data from file
+  const data = await getData(config);
+  // build output object for each post entry
+  const output = buildOutputData(data);
+
+  // return response object
+  return {
+    statusCode: 200,
+    body: JSON.stringify(output)
+  };
 };
---
+++ scripts/cacheData.js
@@ -0,0 +1,54 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+};
+const fs = require('fs');
+
+// init Airtable
+const airtable = require('airtable').base(process.env.AIRTABLE_BASE_ID);
+
+// config
+const config = {
+  url: '/api/',
+  location: 'public/build/',
+  fileName: 'data.json'
+};
+
+// get data from the Airtable API
+const getAirtableData = async (table, view, fields) => {
+  const tableData = await
+    airtable(table)
+      .select({
+        view: view,
+        fields: fields ? fields : []
+      })
+      .all()
+      .catch(error => console.log(error));
+  // only return the necessary field data from tableData
+  return tableData.map((record) => record.fields);
+};
+
+// save data as json to build folder
+const saveData = (data, location, fileName) => {
+  console.log('saving data');
+  try {
+    fs.writeFileSync(config.location + config.fileName, JSON.stringify(data));
+    console.log(`${fileName} saved to ${location}`);
+  }
+  catch (error) {
+    console.log(error);
+  };
+};
+
+// script runner
+const runner = async () => {
+  console.log('starting data cache script');
+  const data = await getAirtableData('credits', 'Grid view');
+  saveData(data, config.location, config.fileName);
+  try {
+    const data = fs.readFileSync(config.location + config.fileName, 'utf-8');
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+runner();
