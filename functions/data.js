3c6ce5d6580329b526306b12b214b5ce33776918
Brian Moore <52761974+zebranode@users.noreply.github.com>
2021-05-01 18:04:02 -0700

    implement file cache of data in build step, conditional use in API (#3)
    * implement file cache of data in build step, conditional use in API
    
    * update data file path
    
    * update data cache file location
    
    * use dynamic path
    
    * update dev script
    
    * implement data cache

--- functions/data.js
+++ functions/data.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
+  require('dotenv').config();
 };
 
 // init Airtable
@@ -7,50 +7,53 @@ const airtable = require('airtable').base(process.env.AIRTABLE_BASE_ID);
 
 // get data from the Airtable API
 const getAirtableData = async (table, view, fields) => {
-  const tableData = await 
+  const tableData = await
     airtable(table)
-    .select({
-      view: view,
-      fields: fields ? fields : []
-      })
-    .all();
+      .select({
+        view: view,
+        fields: fields ? fields : []
+        })
+      .all();
   // only return the necessary field data from tableData
   return tableData.map((record) => record.fields);
 };
 
-exports.handler = async function(event, context) {
-    // get data from the Airtable API
-    const data = await getAirtableData('credits', 'Grid view');
-
-    // build output object for each post entry
-    const output = data.map(entry => {
-
-        const creditData = {
-            title: entry.title,
-            credits: entry.credits,
-        };
-        if (entry.youtube) {
-            creditData.youtube = entry.youtube;
-        };
-        if (entry["bandcamp-url"]) {
-            creditData.bandcamp = {
-                url: entry["bandcamp-url"],
-                albumId: entry["bandcamp-id"]
-            }
-        };
-        if (entry["custom-image"]) {
-            creditData.customMedia = {
-                image: entry["custom-image"],
-                audio: entry["custom-audio"] ? entry["custom-audio"].replace(/\s/g, '').split(",") : null
-            }
-        }
-        
-        return creditData;
-    });
-
-    // return response object
-    return {
-        statusCode: 200,
-        body: JSON.stringify(output)
+const buildOutputData = (rawData) => {
+  // map raw data to desired output object and return it
+  return rawData.map(entry => {
+    // build object properties conditionally
+    const creditData = {
+      title: entry.title,
+      credits: entry.credits,
     };
+    if (entry.youtube) {
+      creditData.youtube = entry.youtube;
+    };
+    if (entry["bandcamp-url"]) {
+      creditData.bandcamp = {
+        url: entry["bandcamp-url"],
+        albumId: entry["bandcamp-id"]
+      }
+    };
+    if (entry["custom-image"]) {
+      creditData.customMedia = {
+        image: entry["custom-image"],
+        audio: entry["custom-audio"] ? entry["custom-audio"].replace(/\s/g, '').split(",") : null
+      }
+    }
+    //return object to map function
+    return creditData;
+  });
+};
+
+exports.handler = async function (event, context) {
+  // get airtable data
+  const data =  await getAirtableData('credits', 'Grid view');
+  // build output object for each post entry
+  const output = buildOutputData(data);
+  // return response object
+  return {
+    statusCode: 200,
+    body: JSON.stringify(output)
+  };
 };
